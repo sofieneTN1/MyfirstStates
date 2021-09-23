814f5c5e9159a0b433ad1c79043baec49b29518d
sofieneTN1 <sofiene.fliss@gmail.com>
2021-09-23 19:37:43 +0300

    first version for checkpoint states

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    
+        constructor(){
+          super()
+
+          this.state = {
+                    shows : true,
+                    Person : {fullName:'Sofiene  FLISS', bio:'11/08/1990',imgSrc:'/myphoto.jpg',profession:'Expert en informatique'}
+          }
+
+          this.clickHandler = this.clickHandler.bind(this);
+
+        } 
+
+
+        clickHandler(){
+            this.state.shows===true? this.setState({shows:false}) : this.setState({shows:true})
+        }
+
+
+
+        render(){
+
+            return(
+
+            <div className="container">
+              <div  style={this.state.shows===true ? {} : { display: 'none' }}>
+                              <img src={this.state.Person.imgSrc} alt="thismyphoto"/>
+                              <h1>{this.state.Person.fullName}</h1>
+                              <h1> {this.state.Person.bio}</h1>
+                              <h3>{this.state.Person.profession}</h3>                   
+              </div>
+              <button onClick={this.clickHandler}>Show/Hide</button>
+            </div> 
+            )           
+        }
+
 }
 
-export default App;
+export default App
